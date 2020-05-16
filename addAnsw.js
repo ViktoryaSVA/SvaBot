90c5e02894285200accd9e61b0d7088ad90fd009
Viktorya SVA <viktorya.scherba@gmail.com>
2020-05-16 16:02:12 +0300

    Commit

--- addAnsw.js
+++ addAnsw.js
@@ -1,3 +1,8 @@
+const { pool } = require('./config.js');
+let ans = [
+    'Смешно','Хорошо','Классно','ООоо','хм','Да','Воу',' ','Эммм...','Пахпахпаххпахп','Мммм','Ладнооо',' ','Я','Неее',
+    'Что?','Всм?','Нужно подумать','Дяяяя'
+];
 let stik = [
     'CAACAgQAAxkBAAKzR169HZTDG8CjAAHG73q7Vlvg43UNugACegEAAnQ4EhCFJKTRsFp-rhkE','CAACAgQAAxkBAAKzVV69HeR6PY2kRk7SSlzGflUQkpu8AAIYAAN0OBIQlihYGsqlvTYZBA', 'CAACAgQAAxkBAAKzWV69HfH_U7-GbrknjShZxtBmkCCFAAIQAQACdDgSEBNohE5YY5-zGQQ', 'CAACAgQAAxkBAAKzW169HfmoNWy_iLKcvJjfVThrs1hGAAIBAAN0OBIQ1m1UN1HE_9YZBA',
     'CAACAgQAAxkBAAKzX169Hgr2hupfZND-cw_wUplAmCh-AAICAAN0OBIQ3Trx4ebQOPQZBA', 'CAACAgQAAxkBAAKzYV69Hheu_uG7sLZQsopRZJx0Moy3AAIFAAN0OBIQNMDuoCteOsUZBA', 'CAACAgQAAxkBAAKzZV69HiHK1kofcNMuKihs1tE209NPAAIGAAN0OBIQcwIO4gNlUGoZBA', 'CAACAgQAAxkBAAKzZ169HipDAgmQIjjYLv50qrfmzLaRAAIHAAN0OBIQFc8OA9yVAnsZBA',
@@ -5,3 +10,42 @@ let stik = [
     'CAACAgQAAxkBAAKzc169HlSgODTvXZYpJ0rf8_krdpJxAAIMAAN0OBIQJDTGZa0ypCUZBA', 'CAACAgQAAxkBAAKzdV69HlsKfmrZHnMNthYqcI_j9gT3AAINAAN0OBIQx-QsWeZZ1psZBA', 'CAACAgQAAxkBAAKzeV69HmR5vkmfYssTT-PLOHKrtpCDAAIOAAN0OBIQCBMdb1xdAXwZBA', 'CAACAgQAAxkBAAKzfV69Hmss5Ds5X8LuPr1K1o5jStnoAAIPAAN0OBIQsMYd4x7uktkZBA',
     'CAACAgQAAxkBAAKzf169HnPnHRq-QJAa8uI0OWUBGhxCAAIQAAN0OBIQ8nF4CIJzQJ4ZBA', 'CAACAgQAAxkBAAKzhF69Hnq2rD3mpD32Pidp77mlIl8rAAIRAAN0OBIQFMCP1GwjG9EZBA', 'CAACAgQAAxkBAAKzh169HoN3R5kmJbzWUDoxN2DPBcm7AAISAAN0OBIQOisBJ-YXynsZBA', 'CAACAgQAAxkBAAKzi169HpWlqH7BoN6uCQJYJqxFBijDAAJwAQACdDgSEK3CorPtsXMLGQQ'
 ];
+const  answer_in = async (item, item2) =>  {
+    let promisePool = await pool.promise();
+    let [rows,fields] = await promisePool.query("INSERT INTO answer SET stik = '" + item + "', ans = '" + item2 + "'");
+    return rows;
+};
+
+
+stik.forEach(function (item) {
+    console.log(item);
+    answer_in(item);
+})
+
+ans.forEach(function (item2) {
+    answer_in(item2);
+})
+
+// const  stik_in = async (item) =>  {
+//     let promisePool = await pool.promise();
+//     let [rows,fields] = await promisePool.query("INSERT INTO answer SET stik = '" + item + "'");
+//     return rows;
+// };
+//
+//
+// stik.forEach(function (item) {
+//     console.log(item);
+//     stik_in(item);
+// })
+//
+// const  answer_in = async (item2) =>  {
+//     let promisePool = await pool.promise();
+//     let [rows,fields] = await promisePool.query("INSERT INTO answer SET ans = '" + item2 + "'");
+//     return rows;
+// };
+//
+// ans.forEach(function (item2) {
+//     answer_in(item2);
+// })
+
+
--- index.js
+++ index.js
@@ -5,29 +5,48 @@ let token = '703135398:AAE_5Nexklq7uACQlO__ABwfb97KmoMwAX0';
 let bot = new TelegramBot(token, {polling: true});
 
 const { pool } = require('./config.js');
-// const  cov_districts = async (item) =>  {
-//     let promisePool = await pool.promise();
-//     let [rows,fields] = await promisePool.query("SELECT name, answers FROM bot " + item + "%'");
-//     //   await pool.close();
-//     return rows;
-// };
-//
 
-let allStikers = require('./stik.js');
+const  answer = async (item) =>  {
+    let promisePool = await pool.promise();
+   // let [rows,fields] = await promisePool.query("SELECT * FROM bot WHERE answers LIKE '%" + item + "%'");
+    let [rows,fields] = await promisePool.query("SELECT * FROM answer WHERE id = 11 ");
+    //   await pool.close();
+    return rows;
+};
 
-bot.on('message', (msg) => {
+
+
+
+
+
+let bot_ansver = async (msg) =>{
+    let answer_s = await answer(msg.text);
 
     let chatId = msg.chat.id;
-    if(msg.text == 'привіт') {
-        pool.query('SELECT name FROM bot', function(error, result, fields){
-            bot.sendMessage(chatId, 'І тобі привіт' + result);
-        });
 
-        bot.sendSticker(chatId, 'CAADAgADCwADAqQnC5FzuTJBvI9wAg');
-    }
+    await answer_s.forEach(function (result) {
+        bot.sendSticker(chatId, result.stik);
+     //   bot.sendMessage(chatId, 'І тобі привіт ' + result.name);
+
+    })
+}
+
+
+
+
+
+
+
+bot.on('message', (msg) => {
+    bot_ansver(msg);
+
+    // if(msg.text == msg.text) {
+    //     pool.query('SELECT name FROM bot', function(error, result, fields){
+    //
+    //     });
+    //
+    //     bot.sendSticker(chatId, 'CAADAgADCwADAqQnC5FzuTJBvI9wAg');
+    // }
 
 });
-
-
-
 
